3e368353e3fb0f572a23d0aa281bf235539314f7
mr.Ivan <wee.portal@mail.ru>
2024-04-17 14:54:56 +0300

    добавил чат

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Chatbot from 'react-chatbot-kit'
+import 'react-chatbot-kit/build/main.css'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ActionProvider from '../ActionProvider.jsx'
+import MessageParser from '../MessageParser.jsx'
+import config from '../config.js'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Chatbot
+			config={config}
+			messageParser={MessageParser}
+			actionProvider={ActionProvider}
+		/>
+	</React.StrictMode>
 )
